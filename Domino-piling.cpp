bdad28c6faa4930ba09635af5f648cf0087ebdac
Mahafuzur Rahaman <m.mahfuz.me@gmail.com>
2020-05-13 23:11:56 +0600

    Create Domino-piling.cpp

---
+++ Domino-piling.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	
+	int m, n, d, r, t;
+	
+	d = 2*1;
+	
+	
+	cin >> m >> n;
+	
+	r = m*n;
+	
+	t = r/d;
+	
+	if(t%2 != 0) {
+		ceil(t);
+	}
+	
+	cout << t;
+	
+	
+	
+	return 0;
+}
